732fe6a3741e1a843d985547e5132900db49db29
ohyeon1002 <skyblueazure4@gmail.com>
2025-04-22 10:44:11 +0900

    small edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <span className="text-2xl font-bold">
-        Now you&apos;re signed in with
+        Now you&#39;re signed in with
         <span className="ml-2 underline italic">
           {localStorage.getItem("loggedAccount")}
         </span>
